4625c3ba798eeb3877c2a69ee70b2192ee3ebeda
Adriano Lourenço <adrianolf2003@gmail.com>
2025-04-28 01:56:40 -0300

    Atualizar política de cache do axios

--- app/javascript/api/index.js
+++ app/javascript/api/index.js
@@ -1,30 +1,28 @@
-import axios from 'axios';
+import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: `${window.location.origin}/api/v1`,
   timeout: 10000,
   headers: {
-    'Content-Type': 'application/json',
-  }
+    "Content-Type": "application/json",
+    "Cache-Control": "no-cache",
+    Pragma: "no-cache",
+    Expires: "0",
+  },
 });
 
-// Interceptor para adicionar o token do Vuex
+// Interceptor para adicionar o token
 apiClient.interceptors.request.use(
   (config) => {
-    config.headers['Authorization'] = localStorage.auth_token;
+    config.headers["Authorization"] = localStorage.auth_token;
     return config;
   },
   (error) => Promise.reject(error)
 );
 
 apiClient.interceptors.response.use(
-  (response) => {
-    return response.data; // Retorna apenas o corpo da resposta
-  },
-  (error) => {
-    // Opcional: Você pode adicionar lógica de tratamento de erros aqui
-    return Promise.reject(error);
-  }
+  (response) => response.data,
+  (error) => Promise.reject(error)
 );
 
 export default apiClient;
