f7a599705e0c7bd73a9d2d4aba7f90535d38bfa9
Adriano Lourenço <adrianolf2003@gmail.com>
2025-04-28 02:09:02 -0300

    Remover arquivos

--- app/controllers/api/v1/inss_calculations_controller.rb
+++
@@ -1,7 +0,0 @@
-class Api::V1::InssCalculationsController < ApplicationController
-  def discount
-    salary = params[:salary].to_f
-    discount = InssDiscountCalculatorService.new.perform(salary)
-    render json: { salary: salary, inss_discount: discount }, status: :ok
-  end
-end
--- app/javascript/api/inss_calculations.js
+++
@@ -1,4 +0,0 @@
-import apiClient from "./index";
-
-export const calculateDiscount = (salary) =>
-  apiClient.post(`/inss_calculations/discount`, { salary });
